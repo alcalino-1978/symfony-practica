091d854fffc0f89c1940a9f6f347fbfa60a8739d
nicolascalise <nicolas.calise@bootcamp-upgrade.com>
2023-03-11 12:01:42 +0100

    Edicion de animales

--- src/Controller/AnimalesController.php
+++ src/Controller/AnimalesController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Animal; 
@@ -28,5 +29,34 @@ class AnimalesController extends AbstractController
         return new RedirectResponse(
             $this->generateUrl('app_animales')
         );
+    }
+
+    #[Route('/animales/editar/{animal}', name: 'app_animales_editar')]
+    public function editar(EntityManagerInterface $entityManager, Animal $animal):Response
+    {
+        return $this->render('animales/edicion.html.twig', [
+            "animal" => $animal
+        ]);
+    }
+
+    #[Route('/animales/guardar/{animal}', name: 'app_animales_guardar', methods:["POST"])]
+    public function guardar(EntityManagerInterface $entityManager, Request $request, Animal $animal):Response
+    {
+        //$request->request->has
+        $animal->setPasos($request->request->getInt("steps",0));
+        $animal->setNombre($request->request->get("name", "Nombre Random"));
+        //$animal->setNacimiento($request->request->get("birthdate", ""));
+        $animal->setNacimiento(
+            new \DateTimeImmutable($request->request->get("birthdate", "")) 
+        );
+
+        $entityManager->persist($animal);
+        $entityManager->flush();
+
+        return new RedirectResponse(
+            $this->generateUrl('app_animales')
+        );
+
+
     }
 }
--- src/DataFixtures/AnimalFixture.php
+++ src/DataFixtures/AnimalFixture.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Animal;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -15,16 +16,25 @@ class AnimalFixture extends Fixture
         $animal1= new Animal();
         $animal1->setNombre('Tiburón Blanco');
         $animal1->setPasos(10);
+        $animal1->setNacimiento(
+            new DateTimeImmutable("2022-05-21 12:34") 
+        );
         $manager->persist($animal1);
 
         $animal2= new Animal();
         $animal2->setNombre('Megalodon');
         $animal2->setPasos(30);
+        $animal2->setNacimiento(
+            new DateTimeImmutable("2023-06-12 15:33") 
+        );
         $manager->persist($animal2);
 
         $animal3= new Animal();
         $animal3->setNombre('Pez Martillo');
         $animal3->setPasos(5);
+        $animal3->setNacimiento(
+            new DateTimeImmutable("2021-02-26 15:11") 
+        );
         $manager->persist($animal3);
         
         $manager->flush();
