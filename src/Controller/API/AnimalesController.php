22bb04f717d554ec2e66db9d0b0c629c1364fb0e
DiegoUpgradeHub <diego.perez@bootcamp-upgrade.com>
2023-03-11 14:39:07 +0100

    Postman. Delete and create function with Status modifications

--- src/Controller/API/AnimalesController.php
+++ src/Controller/API/AnimalesController.php
@@ -7,37 +7,38 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Animal;
 
 #[Route('/api/v1/animales')]
 class AnimalesController extends AbstractController {
+
+    private function objectInfo (Animal $animal) {
+        return [
+            'animal' => $this->generateUrl('api_v1_animales_animal', ['animal' => $animal -> getId()]),
+            'id' => $animal->getId(),
+            'nombre' => $animal->getNombre(),
+            'pasos' => $animal->getPasos(),
+            'nacimiento' => $animal->getNacimiento()?->format('Y-m-d H:i'),
+        ];
+    }
+
     #[Route('/', name: 'api_v1_animales_list', methods:["GET"])]
     public function list(EntityManagerInterface $entityManagerInterface): JsonResponse
     {
         return new JsonResponse( 
-            array_map(function(Animal $animal) { return [
-                'animal' => $this->generateUrl('api_v1_animales_animal', ['animal' => $animal -> getId()]),
-                'id' => $animal->getId(),
-                'nombre' => $animal->getNombre(),
-                'pasos' => $animal->getPasos(),
-                'nacimiento' => $animal->getNacimiento()->format('Y-m-d H:i'),
-            ]; },
+            array_map(fn(Animal $animal) => $this->objectInfo($animal),
             $entityManagerInterface->getRepository(Animal::class)->findAll())
         );
     }
 
     #[Route('/{animal}', name: 'api_v1_animales_animal', methods:["GET"])]
-    public function editar(Animal $animal): JsonResponse {
+    public function detalle(Animal $animal): JsonResponse {
 
         return new JsonResponse(
-            [
-                'id' => $animal->getId(),
-                'nombre' => $animal->getNombre(),
-                'pasos' => $animal->getPasos(),
-                'nacimiento' => $animal->getNacimiento()->format('Y-m-d H:i'),
-            ]
+            $this->objectInfo($animal)
         );
     }
 
@@ -57,13 +58,45 @@ class AnimalesController extends AbstractController {
         $entityManager->flush();
 
         return new JsonResponse(
-            [
-                'animal' => $this->generateUrl('api_v1_animales_animal', ['animal' => $animal->getId()]),
-                'id' => $animal->getId(),
-                'nombre' => $animal->getNombre(),
-                'pasos' => $animal->getPasos(),
-                'nacimiento' => $animal->getNacimiento()->format('Y-m-d H:i'),
-            ]
+            $this->objectInfo($animal), Response::HTTP_CREATED
+        );
+    }
+
+    #[Route('/{animal}', name: 'api_v1_animales_editar', methods:["PATCH"])]
+    public function editar(EntityManagerInterface $entityManager, Request $request, Animal $animal):JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (!empty($data['name'])) {
+            $animal->setNombre($data['name']);
+        }
+
+        if (!empty($data['steps'])) {
+            $animal->setPasos($data['steps']);
+        }
+
+        if (!empty($data['birthdate'])) {
+            $animal->setNacimiento(
+                new \DateTimeImmutable($data['birthdate']) 
+            );
+        }
+
+        $entityManager->persist($animal);
+        $entityManager->flush();
+
+        return new JsonResponse(
+            $this->objectInfo($animal), Response::HTTP_ACCEPTED
+        );
+    }
+
+    #[Route('/{animal}', name: 'api_v1_animales_eliminar', methods:["DELETE"])]
+    public function borrar(EntityManagerInterface $entityManager, Animal $animal):JsonResponse
+    {
+        $entityManager->remove($animal);
+        $entityManager->flush();
+
+        return new JsonResponse(
+            [], Response::HTTP_NO_CONTENT
         );
     }
 
