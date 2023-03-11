98e981e7b9804c15fde84d0af91f3fb8e2a04a81
alberto <alcalino_78@hotmail.com>
2023-03-11 09:28:10 +0100

    styling table animals

--- src/Controller/AnimalesController.php
+++ src/Controller/AnimalesController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use Doctrine\ORM\EntityManagerInterface;
-use App\Entity\Animal;
+use App\Entity\Animal; 
 class AnimalesController extends AbstractController
 {
     #[Route('/animales', name: 'app_animales')]
